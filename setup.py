21414cf07a550e8da0cb196344c50932285287a0
Julien LEICHER <jleicher@gmail.com>
2018-05-23 17:58:06 +0200

    Added long_description

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
+import os
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+README = os.path.join(ROOT_PATH, 'README.md')
+
+with open(README, encoding='utf8') as f:
+  readme = f.read()
 
 setup(
   name='atlas_sdk',
   description='Python SDK to interact with atlas',
+  long_description=readme,
   author='Julien LEICHER',
   license='GPL-3.0',
   version='1.0.0',
